70680436f64795475699ec12a443bde05b6e2515
Brice <bricel67@gmail.com>
2022-05-26 11:32:04 +0200

    Feat - homepage

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-function Button(props) {
-  return <div></div>;
-}
+const Button = ({ bgColor, text, color, borderRadius, size }) => {
+  return (
+    <button
+      type="button"
+      style={{
+        backgroundColor: bgColor,
+        color,
+        borderRadius,
+      }}
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+    >
+      {text}
+    </button>
+  );
+};
 
 export default Button;
